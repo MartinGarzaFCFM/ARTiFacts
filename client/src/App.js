553eabcacc991e963baa4f23fffb10c6e479a4d2
Martin Garza <54215833+MartinGarzaFCFM@users.noreply.github.com>
2023-09-06 14:23:37 -0600

    Paleta de colores
    Agregue la paleta de colores de la pagina y se espera usar en todos sus componentes. Ademas de un nuevo componente que sera nuestra Navbar Principal.

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,28 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 // We import all the components we need in our app
-import Navbar from "./components/navbar";
+import Navbar from "./components/ARTiFacts_Navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
 
+import colors from "./components/colors";
+
 const App = () => {
-    return (
-        <div>
-            <Navbar />
-            <Routes>
-                <Route exact path="/" element={<RecordList />} />
-                <Route path="/edit/:id" element={<Edit />} />
-                <Route path="/create" element={<Create />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        backgroundColor: colors.white,
+      }}
+    >
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<RecordList />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
