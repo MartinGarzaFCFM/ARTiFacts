a528f85c15f192a8fdf6941ec7c6c3f9cb49f9f0
Martin Garza <54215833+MartinGarzaFCFM@users.noreply.github.com>
2023-09-11 03:21:42 -0600

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://artifacts-sz1p.onrender.com/',
+    baseUrl: 'https://artifacts-api-awgx.onrender.com/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
